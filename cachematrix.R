2b684c1b4cc452e85f8763942146b27e2a81fa55
sams-chw <sams.chowdhury@yahoo.com.au>
2015-08-23 14:44:31 +1000

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+# A predefined matrix is supplied while the makeCacheMatrix function is created:
+# a  <-  matrix(c(sample(1:50,4),sample(1:50,4),sample(1:50,4),sample(1:50,4)), nrow = 4, ncol = 4)
+# x <- makeCacheMatrix(a)
+# The supplied matrix can now be modified by accessing the following function:
+# x$setMatrix(matrix(c(sample(1:10,2),sample(1:10,2)), nrow = 2, ncol = 2))
+# cacheSolve function is now called with the output of makeCacheMatrix function as its argument 
+# to obtain the inverse of the matrix either from the cached value or by calculating it:
+# cacheSolve(x)
 
 makeCacheMatrix <- function(x = matrix()) {
   # initially cache is set to NULL
